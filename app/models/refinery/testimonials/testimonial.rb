8ab2f19538e303ecd45f79b696d3d7ca5635aeaf
William Makley <wmakley@tortus.com>
2012-04-27 14:50:49 -0400

    add validation rules and attr_accessible for all attributes

--- app/models/refinery/testimonials/testimonial.rb
+++ app/models/refinery/testimonials/testimonial.rb
@@ -6,7 +6,14 @@ module Refinery
       acts_as_indexed :fields => [:title, :name, :content]
 
       validates :title, :presence => true, :uniqueness => true
-              
+      validates :rating, :numericality => {
+        :only_integer => true,
+        :greater_than_or_equal_to => 0,
+        :less_than_or_equal_to => 5
+      }
+
+      attr_accessible :title, :name, :date, :rating, :content, :hidden, :position
+
     end
   end
 end
